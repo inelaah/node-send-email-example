3cbcf96fc64d85a581499e63541daf9acdc1ae46
Inela Avdic Hukic <inelaah@gmail.com>
2018-11-15 23:16:40 +0100

    clean code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express"),
+let express = require("express"),
   path = require('path'),
   nodeMailer = require('nodemailer'),
   bodyParser = require('body-parser');
 
-var app = express();
+let app = express();
 
 app.use(express.static('src'));
 
@@ -16,19 +16,16 @@ app.post('/send-email', function (req, res) {
       port: 465,
       secure: true,
       auth: {
-          // will be replaced with real "hello" email
+          // should be replaced with real sender's account
           user: 'hello@gmail.com',
-          pass: 'password'
+          pass: 'test'
       }
   });
   let mailOptions = {
-      // will be replaced with real recipient email
-      to: 'hr@gmail.com',
+      // should be replaced with real recipient's account
+      to: 'info@gmail.com',
       subject: req.body.subject,
-      html: '<div><b>Ime:</b>&nbsp;' +  req.body.name +
-        '</div><div><b>Email:</b>&nbsp;' +  req.body.email +
-        '</div><div><b>Telefon:</b>&nbsp;' +  req.body.mobile +
-        '</div><br><div><b>Poruka:</b><br>' +  req.body.message
+      body: req.body.message
   };
   transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -40,7 +37,7 @@ app.post('/send-email', function (req, res) {
   res.end();
 });
 
-var server = app.listen(8081, function(){
-    var port = server.address().port;
+let server = app.listen(8081, function(){
+    let port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
